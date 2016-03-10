13bf1f31adb6b53b343df7c80ef39358b10135d4
Madhuri Patil <madhuri@thoughtworks.com>
2016-03-10 16:42:18 +0530

    The min is splitted into 2 seperate function to read the file & add the guest to the guestlist

--- src/party/application/PrintLabel.java
+++ src/party/application/PrintLabel.java
@@ -8,6 +8,14 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class PrintLabel {
+    private static void addGuestsToList(String file,CreateGuestList guestList) throws IOException{
+        FileReader fr = new FileReader( new File(file));
+        BufferedReader br = new BufferedReader(fr);
+        while(br.readLine() != null){
+            guestList.add(br.readLine());
+        }
+    }
+
     public static void main(String[] args) throws IOException{
         ArgumentSplitter arguments = new ArgumentSplitter(args);
         Printer systemOutPrinter = new TerminalPrinter();
@@ -15,12 +23,9 @@ public class PrintLabel {
         String [] files = arguments.files;
         for (String file : files) {
             CreateGuestList guests = new CreateGuestList(arguments.filters);
-            FileReader fr = new FileReader( new File(file));
-            BufferedReader br = new BufferedReader(fr);
-            while(br.readLine() != null){
-                    guests.add(br.readLine());
-            }
+            addGuestsToList(file,guests);
             systemOutPrinter.print(guests.sortedGuests(),arguments.filters,option[0]);
         }
     }
+
 }
