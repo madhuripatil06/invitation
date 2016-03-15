412e9e6b11b494f06f2e390b055920da94032435
Madhuri Patil <madhuri@thoughtworks.com>
2016-03-15 17:34:12 +0530

    The width for the invitation cove is now based on the size which the gustdata reqiures

--- src/party/design/PlusDashDesign.java
+++ src/party/design/PlusDashDesign.java
@@ -1,39 +1,53 @@
 package party.design;
 
 public class PlusDashDesign implements Design {
-    private String generateDashes(){
+    private int count = 0;
+
+    private String generateDashes(String symbol) {
         String dashes = "";
-        for(int index = 0 ; index <30 ;index++ )
-            dashes += "-";
+        for (int index = 0; index < count - 2; index++)
+            dashes += symbol;
         return dashes;
     }
 
     private String printEntity(String name) {
-        int spaces = 29-name.length();
-        return "| "+name+generateSpaces(spaces)+"|";
+        int spaces = (count-3) - name.length();
+        return "| " + name + generateSpaces(spaces) + "|";
     }
 
     private String generateSpaces(int spaces) {
-        String result="";
-        for(int index = 0 ;index <spaces  ; index++ ){
+        String result = "";
+        for (int index = 0; index < spaces; index++) {
             result += " ";
         }
         return result;
     }
 
-    private String generateBorder(){
-        return  String.format("%s%s%s","+",generateDashes(),"+");
+    private void setCount(String name, String cityState, String country) {
+        count = name.length();
+        if (cityState.length() > count)
+            count = cityState.length();
+        if (country.length() > count)
+            count = country.length();
+        count += 4;
     }
 
-    private String generateMiddleBoder(){
-        return String.format("%s%s%s","|",generateDashes(),"|");
+    private String generateBorder() {
+        return String.format("%s%s%s", "+", generateDashes("-"), "+");
+    }
+
+    private String generateMiddleBoder() {
+        return String.format("%s%s%s", "|", generateDashes("-"), "|");
     }
 
     @Override
     public String DesignPattern(String name, String address) {
-        String [] addressEntities = address.toString().split("\n");
-        String result = String.format("%s\n%s\n%s\n",generateBorder(),printEntity(name),generateMiddleBoder());
-        result += String.format("%s\n%s\n%s\n",printEntity(addressEntities[0]),printEntity(addressEntities[1]),generateBorder());
+        String[] addressEntities = address.toString().split("\n");
+        String cityState = addressEntities[0];
+        String country = addressEntities[1];
+        setCount(name, cityState, country);
+        String result = String.format("%s\n%s\n%s\n", generateBorder(), printEntity(name), generateMiddleBoder());
+        result += String.format("%s\n%s\n%s\n", printEntity(cityState), printEntity(country), generateBorder());
         return result;
     }
 }
